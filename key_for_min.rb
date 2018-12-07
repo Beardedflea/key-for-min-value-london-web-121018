3a2bc7bf8d15762ba9c9251cfc1d060875eec1ba
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-07 20:53:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if block_given?
+    i = -100
+    while i < 600
+      if name_hash.key(i) == nil 
+        i+= 1 
+      else 
+        return name_hash.key(i)
+      end 
+    end
+  else
+    return nil 
+  end
 end
